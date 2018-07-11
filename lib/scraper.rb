6295ff86691d99ebe114462a351e9700fc5d5341
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-11 01:47:39 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,33 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
-  end
+    def self.scrape_index_page(index_url)
+        # binding.pry
+        array = []
+        person = {}
+        Nokogiri::HTML(open(index_url)).css(".roster-cards-container .student-card").each do|eachperson|
+            person = {
+                :name => eachperson.css("h4").text,
+                :location => eachperson.css("p").text,
+                :profile_url => eachperson.css("a").attribute("href").value
+            }
+        end
+        array.push(person)
+        array
+    end
 
-  def self.scrape_profile_page(profile_url)
-    
-  end
-
+    def self.scrape_profile_page(profile_url)
+        # binding.pry
+        profile = {}
+        doc = Nokogiri::HTML(open(profile_url))
+        profile = {
+        :twitter => doc.css(".social-icon-container a")[0].attribute("href").value,
+        :linkedin => doc.css(".social-icon-container a")[1].attribute("href").value,
+        :github => doc.css(".social-icon-container a")[2].attribute("href").value,
+        :blog => doc.css(".social-icon-container a")[3].attribute("href").value,
+        :profile_quote => doc.css("div.profile-quote").text,
+        :bio => doc.css(".description-holder").css("p").text
+        }
+        profile
+    end
 end
-
